75303e49c514f846dc5bd8f7800070f08ae9de97
Thomas Thron <tt@intelligentgraphics.biz>
2019-06-23 11:35:22 +0200

    rename colors.ts to constants.ts

--- src/button.tsx
+++ src/button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import styled from "styled-components";
-import { COLORS } from "./colors";
+import { COLORS } from "./constants";
 
 const ButtonDiv = styled.div`
 	background: ${COLORS.light};
