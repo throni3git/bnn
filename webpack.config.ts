ba79cdd1f80791ee0ee010f7b5174e9861626132
Unknown <throni3git@gmail.com>
2020-03-27 19:02:56 +0100

    add workbox plugin for progressive web apps

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,7 +37,13 @@ const config = {
 				to: "./"
 			},
 			"src/manifest.webmanifest"
-		])
+		]),
+		new WorkboxPlugin.GenerateSW({
+			// these options encourage the ServiceWorkers to get in there fast
+			// and not allow any straggling "old" SWs to hang around
+			clientsClaim: true,
+			skipWaiting: true
+		})
 	],
 	output: {
 		filename: "bundle.[hash].js",
