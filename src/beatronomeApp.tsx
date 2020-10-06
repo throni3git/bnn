ff13088b9b1ffd3a61117f01f5205de4a1311e3b
Thomas Thron <throni3git@gmail.com>
2020-10-06 11:34:31 +0100

    move UI elements to extra file

--- src/beatronomeApp.tsx
+++ src/beatronomeApp.tsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 import { orientation } from "o9n";
 
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import { Range, Direction } from "react-range";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const GlobalStyle = createGlobalStyle`
-	*, *:before, *:after {
-		box-sizing: border-box;
-		outline: none;
-	}
-`;
-
-declare var IS_PRODUCTION: boolean;
 import {
 	subscribe,
 	getState,
@@ -32,167 +23,35 @@ import {
 	resetTimerIfStopped,
 } from "./util";
 import Button from "./button";
-import { DIR_DRUMSETS, DIR_LOOPS, COLORS } from "./constants";
+import { DIR_DRUMSETS, DIR_LOOPS } from "./constants";
 import Matrix from "./matrix";
+import {
+	AllDiv,
+	Heading,
+	ContainerDiv,
+	SliderPadding,
+	FlexRow,
+	Row,
+	FixedRow,
+	Column,
+	FixedColumn,
+	RangeTrackHorizontal,
+	RangeTrackVertical,
+	createRangeThumb,
+	CenteredSmall,
+	CenteredLarge,
+} from "./uiElements";
+
+declare var IS_PRODUCTION: boolean;
+
+const GlobalStyle = createGlobalStyle`
+	*, *:before, *:after {
+		box-sizing: border-box;
+	}
+`;
 
 const bracketsRegEx = /\[[^\]]*\]/;
 const meterRegEx = /\d/;
-
-const AllDiv = styled.div<{ deviceMode: EDeviceMode }>(
-	(props) => `
-	display: flex;
-	overflow: hidden;
-	flex-direction: column;
-	max-height: 1024px;
-	max-width: 1024px;
-	width: 100%;
-	height: 100%;
-	font-size: ${
-		props.deviceMode === EDeviceMode.SmallPortrait ||
-		props.deviceMode === EDeviceMode.SmallLandscape
-			? "0.8em"
-			: null
-	}
-	margin: ${props.deviceMode === EDeviceMode.Desktop ? "auto" : null};
-	border: ${
-		props.deviceMode === EDeviceMode.Desktop
-			? "2px solid " + COLORS.lightBorder
-			: null
-	};
-`
-);
-
-const Heading = styled.div`
-	font-size: 2em;
-	font-weight: bold;
-	justify-content: center;
-	text-align: center;
-	padding-top: 4px;
-`;
-
-const ContainerDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	flex: 1;
-	justify-content: space-evenly;
-	padding: 4px;
-`;
-
-// TODO löschen
-const SliderCaptionDiv = styled.div`
-	display: flex;
-	justify-content: center;
-	/* padding: 3px; */
-`;
-
-const SliderPadding = styled.div`
-	display: flex;
-	flex: 1;
-	padding: 10px;
-`;
-
-const FlexRow = styled.div`
-	/* border: 1px dotted red; */
-	display: flex;
-	flex: 1;
-	flex-direction: row;
-	justify-content: center;
-`;
-
-const Row = styled.div`
-	/* border: 1px dotted red; */
-	display: flex;
-	/* flex: 1; */
-	flex-direction: row;
-	justify-content: space-evenly;
-`;
-
-const FixedRow = styled.div`
-	/* border: 1px dotted darkred; */
-	/* min-height: 40px; */
-	display: flex;
-`;
-
-const Column = styled.div`
-	/* border: 1px dotted green; */
-	display: flex;
-	flex: 1;
-	flex-direction: column;
-	justify-content: center;
-`;
-
-const FixedColumn = styled.div<{ deviceMode: EDeviceMode }>(
-	(props) => `
-	/* border: 1px dotted darkgreen; */
-	display: flex;
-	width: ${
-		props.deviceMode === EDeviceMode.SmallLandscape ||
-		props.deviceMode === EDeviceMode.SmallPortrait
-			? "50px"
-			: "60px"
-	};
-	flex-direction: column;
-	justify-content: center;
-`
-);
-
-const RangeTrackHorizontal = styled.div`
-	height: 20px;
-	width: calc(100% - 20px);
-	margin: auto;
-	position: relative;
-
-	&::before {
-		content: "";
-		background-color: ${COLORS.light};
-		border: 2px solid ${COLORS.lightBorder};
-		display: block;
-		width: calc(100% + 20px);
-		height: 20px;
-		transform: translateX(-10px);
-		position: absolute;
-	}
-`;
-
-const RangeTrackVertical = styled.div`
-	height: calc(100% - 20px);
-	width: 20px;
-	margin: auto;
-
-	&::before {
-		content: "";
-		background-color: ${COLORS.light};
-		border: 2px solid ${COLORS.lightBorder};
-		display: block;
-		width: 20px;
-		height: calc(100% + 20px);
-		transform: translateY(-10px);
-		position: absolute;
-	}
-`;
-
-const createRangeThumb = ({ props }) => (
-	<div
-		{...props}
-		style={{
-			...props.style,
-			height: "20px",
-			width: "20px",
-			backgroundColor: COLORS.fc,
-		}}
-	/>
-);
-
-const CenteredSmall = styled.div`
-	font-size: 0.7em;
-	text-align: center;
-`;
-
-const CenteredLarge = styled.div`
-	font-size: 2em;
-	font-weight: bold;
-	text-align: center;
-`;
 
 export class BeatronomeApp extends React.Component<
 	IBeatronomeAppProps,
